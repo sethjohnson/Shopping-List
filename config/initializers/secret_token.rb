cdf828c83684bb593346599aa68e993ce70dce3e
Seth <seth.johnson@desiringgod.org>
2011-06-14 14:33:00 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shoppinglist::Application.config.secret_token = 'bfa8b0ee7d1f5848ba6785359f6ebb73474b2e31f312dd5814ec234ccedd541e715d07c4cd646702d064ca659989dc8fcd505793f72f89ffff002776abe8d6eb'
